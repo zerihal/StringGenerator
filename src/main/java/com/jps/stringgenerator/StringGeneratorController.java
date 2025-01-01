b254aeaa9148d9ef8cdc298fb18d8f916f6a2c42
John <zerihal@hotmail.com>
2025-01-01 23:35:24 +0000

    Updated string generator endpoint to accept optional parameter for length

--- src/main/java/com/jps/stringgenerator/StringGeneratorController.java
+++ src/main/java/com/jps/stringgenerator/StringGeneratorController.java
@@ -1,18 +1,38 @@
 package com.jps.stringgenerator;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class StringGeneratorController {
 
-    @Autowired
-    StringGeneratorService stringGenService;
+    private final StringGeneratorService stringGenService;
 
+    public StringGeneratorController(StringGeneratorService strGenSvc) {
+        this.stringGenService = strGenSvc;
+    }
+
+    // Endpoint for generating a random string of default length (50 chars) or specified length
     @GetMapping("/generate")
-    public String getRandomString() {
+    public String getRandomString(HttpServletRequest request) {
+        String query = request.getQueryString();
+
+        if (query != null) {
+            try {
+                int length = Integer.parseInt(query);
+                return stringGenService.generateRandomString(length);
+            } catch (NumberFormatException e) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid query parameter (integer expected)");
+            }
+        }
+        
         return stringGenService.generateRandomString();
     }
-    
 }
